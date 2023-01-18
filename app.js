b3564f257d487b796c944a840ccc389205ce6e82
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-18 12:30:48 -0800

    finshed basic layout and starting grabbing dom elements and working on event listener

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 /* Imports */
 
 /* Get DOM Elements */
-const playerHp = document.getElementById('player-hp');
+const playerHpEl = document.getElementById('player-hp');
 const enemyInput = document.getElementById('enemy-input');
-const enemyButton = document.getElementById('');
+const enemyButton = document.getElementById('add-enemy-button');
+const defeatedEnemiesEl = document.getElementById('defeated-enemies');
 
 /* State */
-
+let enemiesDefeated = 0;
+let playerHp = 20;
+const enemies = [
+    { name: 'Sonny', hp: 5 },
+    { name: 'Vito', hp: 7 },
+];
 /* Events */
-
+enemyButton.addEventListener('click', () => {
+    const enemyName = enemyInput.value;
+    const addEnemy = {
+        name: enemyName,
+        hp: Math.ceil(Math.random() * 10),
+    };
+    enemies.push(addEnemy);
+    enemyInput.value = '';
+});
 /* Display Functions */
-
+function displayEnemies() {}
 // (don't forget to call any display functions you want to run on page load!)
